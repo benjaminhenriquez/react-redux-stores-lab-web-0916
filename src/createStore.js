f63de86b885fb8905b8c4f462b4300ae85ec8865
github username <github email address>
2016-11-21 17:17:20 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,3 +1,57 @@
 export default function createStore(reducer){
-// your code here!
+  let state = []
+  let listeners = []
+
+  const getState = (reducer) => {
+   return state
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+   }
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+  }
+
+  return {
+   getState: getState,
+   dispatch: dispatch,
+   subscribe: subscribe
+  }
 }
+
+
+// export default function createStore(reducer){
+// // your code here!
+//   let state =[]
+//   let listeners = [];
+//
+//   function getState(reducer){
+//     return state;
+//   };
+//
+//
+//   function dispatch(action){
+//     let state = reducer(state, action);
+//     listeners.forEach(function(listener){listener()})
+//   };
+//
+//
+//   function subscribe(listener){
+//     listeners.push(listener);
+//   };
+//
+//
+//   return{
+//     dispatch: dispatch,
+//     getState: getState,
+//     subscribe: subscribe
+//     }
+//
+// }
+//
+
+
+// store.dispatch({type: 'DECREMENT_COUNT'})
